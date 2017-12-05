721be13ee41fa1038ba5fe6b8258983d2b881abf
Ruslan Filipenko <ruslan.filipenko@evrythng.com>
2017-12-05 17:13:04 +0300

    moved all the stuff from boot to main

--- boot.py
+++ boot.py
@@ -1,29 +1,5 @@
-import gc
 import os
-import config
-import provision
 from machine import UART
-from reset import ResetButton
-from ota_upgrade import start_upgrade_if_needed
-
-
-ResetButton('P19')
 
 uart = UART(0, 115200)
 os.dupterm(uart)
-
-provision.check_and_start_provisioning_mode()
-
-try:
-    config.read_configs()
-except config.InvalidWifiConfigException:
-    print('reading wifi config failed, starting provisioning mode')
-    provision.start_provisioning_server()
-except config.InvalidCloudConfigException:
-    print('reading cloud config failed, starting provisioning mode')
-    provision.start_provisioning_server()
-finally:
-    gc.collect()
-
-start_upgrade_if_needed()
-gc.collect()
--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import gc
+import config
+import provision
+import ota_upgrade
+from reset import ResetButton
+
+ResetButton('P19')
+
+provision.check_and_start_provisioning_mode()
+
+try:
+    config.read_configs()
+except config.InvalidWifiConfigException:
+    print('reading wifi config failed, starting provisioning mode')
+    provision.start_provisioning_server()
+except config.InvalidCloudConfigException:
+    print('reading cloud config failed, starting provisioning mode')
+    provision.start_provisioning_server()
+finally:
+    gc.collect()
+
+ota_upgrade.start_upgrade_if_needed()
+gc.collect()
+
+
 from time import sleep
 from _thread import start_new_thread
 from math import floor
 from machine import WDT, Timer
-from config import cloud_config
 from notification_queue import NotificationQueue
 from accelerometer_sensor import VibrationSensor
 from ambient_sensor import AmbientSensor
 from dispatcher import CloudDispatcher
 from http_notifier import HttpNotifier
-from ota_upgrade import check_and_upgrade_if_needed
 from version import version
+
 
 wdt = WDT(timeout=25000)  # enable it with a timeout of 25 seconds
 queue = NotificationQueue()
-notifier = HttpNotifier(cloud_config['thng_id'], cloud_config['api_key'])
+notifier = HttpNotifier(config.cloud_config['thng_id'], config.cloud_config['api_key'])
 dispatcher = CloudDispatcher(queue, [notifier])
 ambient = AmbientSensor(queue, 90)
 uptimer = Timer.Chrono()
@@ -41,5 +64,7 @@ while True:
 
     firmware_counter -= 1
     if not firmware_counter:
-        check_and_upgrade_if_needed(cloud_config['thng_id'], cloud_config['api_key'])
+        ota_upgrade.check_and_upgrade_if_needed(
+            config.cloud_config['thng_id'],
+            config.cloud_config['api_key'])
         firmware_counter = firmware_period
