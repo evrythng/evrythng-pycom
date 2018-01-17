a6a1ef717d78bf4ca0dcb778b0a94a798b444b1d
Ruslan Filipenko <ruslan.filipenko@evrythng.com>
2018-01-17 11:20:42 +0300

    reviewed support for sigfox

--- sigfox_notifier.py
+++ sigfox_notifier.py
@@ -5,6 +5,13 @@ import ubinascii as binascii
 
 
 class SigfoxNotifier():
+    V_START = 1
+    V_STOP = 2
+    VER = 4
+    TEMP = 5
+    HUM = 6
+    PRES = 7
+    VOLT = 8
 
     def __init__(self):
         self._sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
@@ -18,15 +25,35 @@ class SigfoxNotifier():
 
     def handle_notifications(self, notifications):
         # self._socket.send(bytes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
+        msg = bytearray()
         for n in notifications:
             if n.type == NotificationQueue.VIBRATION_STARTED:
-                pass
+                msg.append(self.V_START)
+                print(msg)
+                self._socket.send(msg)
+
             elif n.type == NotificationQueue.VIBRATION_STOPPED:
-                print('sigfox vibration duration: {}'.format(n.data))
-                self._socket.send(n.data.to_bytes(8, 'little'))
-            elif NotificationQueue.AMBIENT:
-                pass
-            elif NotificationQueue.UPTIME:
-                pass
+                msg.append(self.V_STOP)
+                msg.extend(n.data.to_bytes(8, 'little'))
+                print(msg)
+                self._socket.send(msg)
+
+            elif n.type == NotificationQueue.AMBIENT:
+                print(n.data)
+                msg.append(self.TEMP)
+                msg.extend(int(n.data[0]).to_bytes(8, 'little'))
+                print(msg)
+                self._socket.send(msg)
+                '''
+                t = zip((self.TEMP, self.HUM, self.PRES, self.VOLT), n.data)
+                for a, b in t:
+                    msg.append(a)
+                    msg.extend(int(b).to_bytes(8, 'little'))
+                    print(msg)
+                    self._socket.send(msg)
+                    msg = bytearray()
+                '''
             else:
                 print('unsupported event {}'.format(n.type))
+
+            msg = bytearray()
