fdd835b440948df696eaafc225f5567568b79423
Ruslan Filipenko <ruslan.filipenko@evrythng.com>
2018-01-17 20:30:56 +0300

    change message format for sigfox

--- sigfox_notifier.py
+++ sigfox_notifier.py
@@ -5,55 +5,39 @@ import ubinascii as binascii
 
 
 class SigfoxNotifier():
-    V_START = 1
-    V_STOP = 2
-    VER = 4
-    TEMP = 5
-    HUM = 6
-    PRES = 7
-    VOLT = 8
-
     def __init__(self):
         self._sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
         self._socket = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW)
         self._socket.setblocking(True)
         self._socket.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
+        self._temp = 0
+        self._last_use = 0
+        self._in_use = False
         print('sigfox mac: {}, pac: {}, id: {}'.format(
             binascii.hexlify(self._sigfox.mac()),
             binascii.hexlify(self._sigfox.pac()),
             binascii.hexlify(self._sigfox.id())))
 
     def handle_notifications(self, notifications):
-        # self._socket.send(bytes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
-        msg = bytearray()
         for n in notifications:
+            msg = bytearray()
+            self._last_use = 0
             if n.type == NotificationQueue.VIBRATION_STARTED:
-                msg.append(self.V_START)
-                print(msg)
-                self._socket.send(msg)
+                self._in_use = True
 
             elif n.type == NotificationQueue.VIBRATION_STOPPED:
-                msg.append(self.V_STOP)
-                msg.extend(n.data.to_bytes(8, 'little'))
-                print(msg)
-                self._socket.send(msg)
+                self._in_use = False
+                self._last_use = n.data
 
             elif n.type == NotificationQueue.AMBIENT:
-                print(n.data)
-                msg.append(self.TEMP)
-                msg.extend(int(n.data[0]).to_bytes(8, 'little'))
-                print(msg)
-                self._socket.send(msg)
-                '''
-                t = zip((self.TEMP, self.HUM, self.PRES, self.VOLT), n.data)
-                for a, b in t:
-                    msg.append(a)
-                    msg.extend(int(b).to_bytes(8, 'little'))
-                    print(msg)
-                    self._socket.send(msg)
-                    msg = bytearray()
-                '''
+                self._temp = int(n.data[0])
+
             else:
                 print('unsupported event {}'.format(n.type))
+                continue
 
-            msg = bytearray()
+            msg.append(self._in_use)
+            msg.extend(self._last_use.to_bytes(4, 'little'))
+            msg.extend(self._temp.to_bytes(4, 'little'))
+            print(list(msg))
+            self._socket.send(msg)
